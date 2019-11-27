892acf279a75d9b94fa2ba053a92b9d2fc7cb8c9
Mike <yanov.m.v@gmail.com>
2019-11-28 00:41:38 +0200

    commit

--- lesson_2/apples.py
+++ lesson_2/apples.py
@@ -1,6 +1,8 @@
 n = int(input('Enter number of students: '))
 k = int(input('Enter number of apples: '))
-
-apples = k // n
-basket = k % n
-print('Each student will take ', apples, 'apple(s) and ', basket, 'apple(s) in basket')
+if k > n:
+    apples = k // n
+    basket = k % n
+    print('Each student will take ', apples, 'apple(s) and ', basket, 'apple(s) will be in the basket')
+else:
+    print('You have no enough apples')
