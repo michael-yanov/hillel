8da328629ea40ca620429b96ec7855d413fb3e5f
Mike Yanov <mixa.odessa@gmail.com>
2019-11-28 22:12:03 +0200

    commit

--- lesson_2/apples.py
+++ lesson_2/apples.py
@@ -1,6 +1,6 @@
 n = int(input('Enter number of students: '))
 k = int(input('Enter number of apples: '))
-if k > n:
+if k >= n:
     apples = k // n
     basket = k % n
     print('Each student will take ', apples, 'apple(s) and ', basket, 'apple(s) will be in the basket')
