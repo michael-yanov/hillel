62b84ecf03db977be55991ebd06a10687c8b01fc
Mike Yanov <mixa.odessa@gmail.com>
2019-11-28 23:30:42 +0200

    commit

---
+++ lesson_2/desks.py
@@ -0,0 +1,16 @@
+'''
+Desks and students
+'''
+
+first_class = int(input('Enter number of students in the first class: '))
+second_class = int(input('Enter number of students in the second class: '))
+third_class = int(input('Enter number of students in the third class: '))
+
+num = first_class + second_class + third_class
+desks = num // 2
+
+if (num % 2) == 0:
+    print('Total number of students  is', num,'.' ' You need to buy ', desks, 'desks for all classes')
+else:
+    desks += 1
+    print('Total number of students  is', num,'.' ' You need to buy ', desks, 'desks for all classes')
