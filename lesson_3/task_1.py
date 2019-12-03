4de086479697438e136a5025adb29acfb629de04
Mike Yanov <mixa.odessa@gmail.com>
2019-12-03 22:26:21 +0200

    commit

--- lesson_3/task_1.py
+++ lesson_3/task_1.py
@@ -5,6 +5,5 @@ Square of integer numbers
 n = int(input('Enter the number: '))
 i = 1
 while (i ** 2) <= n:
-    #print(i ** 2)
-    print('{0} {1}'.format(n, i ** 2), end = ' ')
+    print(n,'{:1d}'.format(i ** 2), end='')
     i += 1
