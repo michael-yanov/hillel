c600f6672501e84051d4c488b6a07062a40b738c
Mike <yanov.m.v@gmail.com>
2019-12-05 23:11:01 +0200

    commit

--- lesson_4/task_2.py
+++ lesson_4/task_2.py
@@ -1,3 +1,7 @@
-stroka = 'How are you doing bro?'
+'''
+Дана строка, состоящая из слов, разделенных пробелами. Определите, сколько в ней слов. Используйте для решения
+задачи функцию `count`
+'''
 
+stroka = 'How are you doing bro?'
 print('This sentenсe has {0} words'.format(stroka.count(' ') + 1))
---
+++ lesson_4/task_3.py
@@ -0,0 +1,7 @@
+s = input('Enter word(s): ')
+ch = input('Enter symbol: ')
+l = 0
+for i in :
+    if s[i] == ch:
+        l += 1
+        print(l)
