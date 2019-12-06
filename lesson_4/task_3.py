e368085e4a258b340d55fcf71667c009f2e6400b
Mike Yanov <mixa.odessa@gmail.com>
2019-12-06 16:01:22 +0200

    commit

--- lesson_4/task_3.py
+++ lesson_4/task_3.py
@@ -1,7 +1,12 @@
-s = input('Enter word(s): ')
-ch = input('Enter symbol: ')
+'''
+Пользователь вводит, отдельно, строку `s` и один символ `ch`. Необходимо выполнить поиск в строке `s` всех символов `ch`.
+Для решения можно использовать только функцию `find`(rfind), операторы  `if` и `for`(while).
+'''
+
+s = input('Enter the string: ')
+ch = input('Enter symbol for searching: ')
 l = 0
-for i in :
+for i in range(len(s)):
     if s[i] == ch:
         l += 1
-        print(l)
+print('Found {0} symbols'.format(l))
