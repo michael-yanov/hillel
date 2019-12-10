b9bffe764c0a75b1260ff97e7441b5c2643c53ea
Mike <yanov.m.v@gmail.com>
2019-12-10 20:19:58 +0200

    commit

---
+++ lesson_4/min_max.py
@@ -0,0 +1,5 @@
+min_valur = 0
+max_value = 0
+
+while True:
+    x = int()
---
+++ lesson_5/list.py
@@ -0,0 +1,10 @@
+lst = []
+i = 0
+while i < 5:
+    num = int(input('Enter eight numbers: '))
+    lst.append(num)
+    i += 1
+print(lst)
+print(sum(lst))
+print(min(lst))
+print(max(lst))
