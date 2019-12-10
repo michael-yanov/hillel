a709ac6781737e611ab427ab6d579f2f7ff717c6
Mike <yanov.m.v@gmail.com>
2019-12-10 23:00:12 +0200

    commit

--- lesson_5/rand.py
+++ lesson_5/rand.py
@@ -1,10 +1,10 @@
 import random
 
-down = int(input('Enter down border: '))
-up = int(input('Enter up border: '))
-indx = int(input('Enter type of value: 1 - integer, 2 - float: '))
-while True:
-    if indx == 1:
-        print(random.randint(down, up))
-    else:
-        print(round(random.random() * (up - down) + down, 2))
+lst = []
+i = 0
+while i < 100:
+     lst.append(round(random.random() * 100, 2))
+     lst.sort()
+     i += 1
+print(lst)
+
