691aa143b06a97b01e7315c67a8f82eed2b0f532
Mike Yanov <mixa.odessa@gmail.com>
2019-12-14 10:34:01 +0200

    commit

--- lesson_6/task_dict2.py
+++ lesson_6/task_dict2.py
@@ -1,4 +1,8 @@
-s = 'Lorem Ipsum Lorem Ipsum Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+'''
+Дан текст состоящий из нескольких строки. Выведите слово, которое в этом тексте встречается чаще всего. Если таких слов несколько, выведите последнее.
+'''
+
+s = 'Lorem Ipsum Lorem Lorem Ipsum is simply dummy dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
 s = s.split()
 d = {}
 for i in s:
@@ -9,4 +13,4 @@ for i in s:
 max_word = max(d.values())
 for key, value in d.items():
     if value == max_word:
-        print('The most used word is: ', key, '-', value)
+        print('The most used word is: ', key, '-', value, 'times')
