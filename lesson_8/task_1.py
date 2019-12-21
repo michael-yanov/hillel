a819e3ea858bb45b2647fbc3b9a1ea6fb0760340
Mike Yanov <mixa.odessa@gmail.com>
2019-12-21 17:51:39 +0200

    commit

--- lesson_8/task_1.py
+++ lesson_8/task_1.py
@@ -5,14 +5,28 @@ orders = [
     [88112, 'Einfuhrung in Python3, Berbd Klein', 3, 24.99]
 ]
 
-for i in range(4):
+orders_price = []
+orders_num = []
+
+orders_num = [i[0] for i in orders if len(i)]
+
+
+for i in range(len(orders)):
     price = 0
-    for j in range(4):
-        price = orders[i][2] * orders[i][3]
-        print(orders[i][j], end=' ')
-        orders.append(price)
-    print()
-print(orders)
+    for j in range(len(orders)):
+        price = int(orders[i][2] * orders[i][3])
+        if price < 100:
+            price += 10
+    orders_price.append(price)
+    #print()
+orders_new = tuple(orders_price)
+
+print(orders_num)
+print(orders_price)
+
+total_orders = list(map(lambda x,y: (x,y), orders_num, orders_price))
+print(total_orders, sep='\n')
+
 
 
 
