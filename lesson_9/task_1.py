8397ce7912cd76932dd7ae072e477ba282212e62
Mike Yanov <mixa.odessa@gmail.com>
2020-01-01 15:26:03 +0200

    commit

--- lesson_9/task_1.py
+++ lesson_9/task_1.py
@@ -1,11 +1,17 @@
+'''
+Написать функцию `XOR_cipher`, принимающая 2 аргумента: строку, которую нужно зашифровать, и ключ шифрования, которая
+возвращает строку, зашифрованную путем применения операции XOR (битовое исключающее ИЛИ) над символами строки с ключом.
+Написать также функцию `XOR_uncipher`, которая по зашифрованной строке и ключу восстанавливает исходную строку.
+'''
+
 def XOR_cipher(string, key):
     encrypt_string = []
     for i in string:
         encrypt_string.append(chr(ord(i) ^ key))
     return ''.join(encrypt_string)
 
-string = 'hello guys'
-key = 8
-print('encrypt string is: ', XOR_cipher(string, 8))
+string = 'hello'
+key = 5
+print('encrypt string is: ', XOR_cipher(string, key))
 encrypt_string = XOR_cipher(string, key)
 print('original string is: ', XOR_cipher(encrypt_string, key))
