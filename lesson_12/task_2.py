8f91d417b3cc014a416afec9c2e841f057b12f42
Mike <yanov.m.v@gmail.com>
2020-01-23 17:35:28 +0200

    commit

--- lesson_12/task_2.py
+++ lesson_12/task_2.py
@@ -1,21 +1,18 @@
-<<<<<<< HEAD
-
-
-
-=======
 '''
 Имеется строка вида: AABABBAABBBAB. Необходимо написать функцию которая заменит буквы A на Bб а B, соответственно, на A.
 В результате применения функции к исходной строке, функция должна вернуть строку: BBABAABBAAABA
 '''
 
-string = 'AABABBAABBBAB'
-string_new = []
 
-for i in range(len(string)):
-    if string[i] == 'A':
-        string_new.append('B')
-    else:
-        string_new.append('A')
-print(string)
-print(''.join(string_new))
->>>>>>> github/master
+def change_letters(string):
+    string_new = []
+    for i in range(len(string)):
+        if string[i] == 'A':
+            string_new.append('B')
+        else:
+            string_new.append('A')
+    return ''.join(string_new)
+
+
+print(change_letters('AABABBAABBBAB'))
+
